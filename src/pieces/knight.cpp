7ae49ee57b6a9bd52619ec60f39590b1fd732eb5
Tim <timcarlsson0203@gmail.com>
2025-06-07 23:47:35 +0200

    Added basic knight movement

--- src/pieces/knight.cpp
+++ src/pieces/knight.cpp
@@ -1,4 +1,5 @@
 #include "pieces/knight.hpp"
+#include "board.hpp"
 
 
 Knight::Knight(Color color) : Piece(color) {}
@@ -16,7 +17,35 @@ const sf::Texture& Knight::getTexture(const TextureManager& manager) const
 std::vector<BoardPosition> Knight::getValidMoves(ChessBoard& board, const BoardPosition& pos)
 {
     std::vector<BoardPosition> validMoves = {};
+    
+    int x = pos.x();
+    int y = pos.y();
+    std::vector<BoardPosition> moveOffsets =
+    {
+        {x - 2, y - 1},
+        {x - 2, y + 1},
+        
+        {x - 1, y - 2},
+        {x - 1, y + 2},
+        
+        {x + 1, y - 2},
+        {x + 1, y + 2},
+        
+        {x + 2, y - 1},
+        {x + 2, y + 1}
+    };
 
+    for (BoardPosition offset : moveOffsets)
+    {
+        if (board.isInBounds(offset) && !board.isPiece(offset))
+        {
+            validMoves.push_back(offset);
+        }
+        else if (board.isInBounds(offset) && board.at(pos)->getColor() != board.at(offset)->getColor())
+        {
+            validMoves.push_back(offset);
+        }
+    }
 
     return validMoves;
 }
